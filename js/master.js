e524f1cf711c756f5985da3c9a2bc107f454452d
Hdev <127256453+Hidemi0sski@users.noreply.github.com>
2023-03-25 11:03:59 -0300

    Primeiro commit do site

---
+++ js/master.js
@@ -0,0 +1,30 @@
+function enviar(){
+	var valor = document.getElementById("textinput").value;
+	var valores = document.getElementById("senhainput").value;
+	if (valor == "4MN3S1C0@gmail.com" && valores == "M3M0R145"){
+		document.form1.action="k.html";
+	}
+	else if (valor == "ThePatoIrado@gmail.com" && valores == "tenhomedodepatos"){
+		document.form1.action="l.html";
+	} 
+	else if (valor == "Gabzinhadocabelovermelho@gmail.com" && valores == "gabshine0729eéisso"){
+		document.form1.action="g.html";
+	} 
+	else if (valor == "IrmaosS@gmail.com" && valores == "GibsonS19"){
+		document.form1.action="mk.html";
+	} 
+	else if (valor + valores){
+		document.form1.action="home.html";
+	} 
+}
+	
+window.sr = ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.area-1', {duration: 2000});
+ScrollReveal().reveal('.area-2', {duration: 3000});
+ScrollReveal().reveal('.scroll-index', {duration: 1000});
+ScrollReveal().reveal('.scroll-index1', {duration: 1000});
+ScrollReveal().reveal('.scroll-index2', {duration: 1000});
+ScrollReveal().reveal('.scroll-index3', {duration: 1000});
+
+	
